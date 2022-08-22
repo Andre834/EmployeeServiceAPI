e4e88fdd49d64fa2541774c48c5eb331b746cbad
ANDRÉ OLIVEIRA DOS SANTOS <andreoliveiradossantos@outlook.com.br>
2022-08-22 01:10:47 -0300

    Add project files.

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,64 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+public class EmployeesController : ApiController
+{
+    public HttpResponseMessage Get()
+    {
+        using (EmployeeDBContext dbContext = new EmployeeDBContext())
+        {
+            var Employees = dbContext.Employees.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, Employees);
+        }
+    }
+    public HttpResponseMessage Get(int id)
+    {
+        using (EmployeeDBContext dbContext = new EmployeeDBContext())
+        {
+            var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            if (entity != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, entity);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    "Employee with ID "  + id.ToString() + " not found");
+            }
+        }
+    }
+
+    //public void Post([FromBody] Employee employee)
+    //{
+    //    using (EmployeeDBContext dbContext = new EmployeeDBContext())
+    //    {
+    //        dbContext.Employees.Add(employee);
+    //        dbContext.SaveChanges();
+    //    }
+    //}
+
+    public HttpResponseMessage Post([FromBody] Employee employee)
+    {
+        try
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                dbContext.Employees.Add(employee);
+                dbContext.SaveChanges();
+                var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                message.Headers.Location = new Uri(Request.RequestUri +
+                    employee.ID.ToString());
+                return message;
+            }
+        }
+        catch (Exception ex)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+        }
+    }
+
+}
---
+++ EmployeeService/Helpers/MethodsHttpIntrodoction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeService.Helpers
+{
+    public class MethodsHttpIntrodoction
+    {
+        /*
+         * ---------------------------------------------------------------------------------
+         *  INTRODUÇÃO AOS VERBOS HTTP E AS BOAS PRÁTICAS DE UMA API RESTFULL
+         * ----------------------------------------------------------------------------------
+         * 
+            Pontos a serem lembrados ao trabalhar com o método de postagem da API da Web:
+            1- Se um tipo de retorno de método for nulo na Web API, por padrão, 
+            a API Service retornará o código de status 204 No Content.
+
+            2- Quando um novo item é criado, devemos retornar o código de status 201 Item Created.
+            Com o código de status 201, também devemos incluir o local, ou seja, URI do item recém-criado.
+        
+            3- Quando um item não for encontrado, em vez de retornar NULL e o código de status 200 OK, 
+            retorne o código de status 404 Not Found juntamente com uma mensagem significativa como “ 
+            Funcionário com ID = 15 não encontrado ”
+         
+         
+         */
+    }
+}
