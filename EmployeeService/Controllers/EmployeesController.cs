af626e8455e6d04d81f2dca7e9bdafce18dd711e
ANDRÉ OLIVEIRA DOS SANTOS <andreoliveiradossantos@outlook.com.br>
2022-08-22 22:50:58 -0300

    Adicionados os metodos PUT/DELETE

--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -32,14 +32,7 @@ public class EmployeesController : ApiController
         }
     }
 
-    //public void Post([FromBody] Employee employee)
-    //{
-    //    using (EmployeeDBContext dbContext = new EmployeeDBContext())
-    //    {
-    //        dbContext.Employees.Add(employee);
-    //        dbContext.SaveChanges();
-    //    }
-    //}
+  
 
     public HttpResponseMessage Post([FromBody] Employee employee)
     {
@@ -60,5 +53,69 @@ public class EmployeesController : ApiController
             return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
         }
     }
+
+
+    
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBContext dbContext = new EmployeeDBContext())
+                {
+                    var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Employee with Id " + id.ToString() + " not found to update");
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        dbContext.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    
+   
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBContext dbContext = new EmployeeDBContext())
+                {
+                    var entity = dbContext.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Employee with Id = " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        dbContext.Employees.Remove(entity);
+                        dbContext.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    
+
+
 
 }
